b11929dcb6b865184d6750c9c3b159fb1599297c
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-09-30 11:42:50 -0300

    Chore: guard is not working

--- src/app/[locale]/_components/client-component.tsx
+++ src/app/[locale]/_components/client-component.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { io } from 'socket.io-client';
+// import { useMemo } from 'react';
+// import { io } from 'socket.io-client';
 
 import { useTranslations } from '@/contexts/translations.context';
 import { LanguageDropdown } from '@/components/tools/language-dropdown';
@@ -8,25 +9,29 @@ import { LanguageDropdown } from '@/components/tools/language-dropdown';
 export function ClientComponent() {
   const { translation } = useTranslations();
 
-  const socket = io('http://localhost:5000/server/condominium-chat', {
-    transports: ['websocket'],
-    auth: {
-      token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMxY2E5YzRkLTg1OWUtNGE1OC1hNWVhLTlkNTlmMGQxNGE1MiIsImlhdCI6MTcyNzUzNTgyNywiZXhwIjoxNzI3NTU3NDI3LCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAifQ.e-XlDVN38sLxOcKaIGT7hTGiowiner1zyc3bqh1Srtg`,
-    },
-    withCredentials: true,
-  });
+  // const socket = useMemo(
+  //   () =>
+  //     io('ws://localhost:5000/server/condominium-chat', {
+  //       transports: ['websocket'],
+  //       auth: {
+  //         token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwZGY0ZjlkLTc4NmUtNGM2MS1hMzBhLTRiNDE4YjNhOWVkOCIsImlhdCI6MTcyNzcwNjkyOSwiZXhwIjoxNzI3NzI4NTI5LCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAifQ.T5D-8wmAsPyhWQQm5Tday6dMRufGuozay6_zAyQmKls`,
+  //       },
+  //       withCredentials: true,
+  //     }),
+  //   []
+  // );
 
-  socket.on('connect', () => {
-    console.log(
-      'Connected to WebSocket with JWT token in Authorization header'
-    );
-  });
+  // socket.on('connect', () => {
+  //   console.log(
+  //     'Connected to WebSocket with JWT token in Authorization header'
+  //   );
+  // });
 
-  socket.on('disconnect', () => {
-    console.log(
-      'Disconected to WebSocket with JWT token in Authorization header'
-    );
-  });
+  // socket.on('disconnect', () => {
+  //   console.log(
+  //     'Disconected to WebSocket with JWT token in Authorization header'
+  //   );
+  // });
 
   return (
     <div className="mt-4 border rounded-sm p-3">
@@ -34,8 +39,6 @@ export function ClientComponent() {
         {translation('common_texts.client_component')}
       </h3>
       <LanguageDropdown />
-
-      <span>{socket.active}</span>
     </div>
   );
 }
